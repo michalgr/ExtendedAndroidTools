8deb390d665e274446fb88ba35b48bfafe86ea05
Michal Gregorczyk <michalgr@live.com>
2022-08-24 18:10:38 +0200

    make clean should remove buck-out

--- toolchain/buck.mk
+++ toolchain/buck.mk
@@ -1,6 +1,14 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
 BUCK = /opt/buck
+BUCK_OUT = buck-out
+
+clean: clean-buck-out
+
+clean-buck-out:
+	-rm -fr $(BUCK_OUT)
+
+.PHONY: clean-buck-out
 
 out/buckconfig: $(OUT_DIR)
 	cp toolchain/buckconfig.template $@
